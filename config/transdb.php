0ed815d51c4085596114dde8ad85c454ede0b225
Mark Leidman <marksleidmans@flynowpaylater.com>
2022-11-22 16:46:45 +0200

    translations path updated

--- config/transdb.php
+++ config/transdb.php
@@ -11,5 +11,5 @@ return [
      * usually located in resources/lang but you may change that.
      */
 
-    'path' => realpath(base_path('resources/lang')),
+    'path' => realpath(base_path('lang')),
 ];
